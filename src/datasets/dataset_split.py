2411eb2892aea224f28ea124380477a3d1c94640
NikolaP454 <97197562+NikolaP454@users.noreply.github.com>
2025-05-24 16:43:17 +0200

    Added Test Dataset Splitting

--- src/datasets/dataset_split.py
+++ src/datasets/dataset_split.py
@@ -1,3 +1,4 @@
+import random
 from tqdm import tqdm
 from typing import Tuple
 
@@ -93,4 +94,109 @@ def generate_train_dataset(
     return Data(
         x=dataset.x[train_mask],
         edge_index=new_edge_index.contiguous(),
+    )
+
+
+def generate_test_dataset(
+    dataset: Data,
+    negative_edges_per_node: int = 10,
+) -> Data:
+    """
+    Generates a test dataset with the specified indices.
+
+    Args:
+        dataset (Data): The PyTorch Geometric dataset object.
+        negative_edges_per_node (int): The number of negative edges to generate per node.
+
+    Returns:
+        Data: A new Data object with the original data and the generated test dataset.
+    """
+
+    if not hasattr(dataset, "test_mask"):
+        raise ValueError("The dataset does not have a test_mask attribute.")
+
+    test_mask = dataset.test_mask
+
+    # Create a new edge index based on the mapping
+    new_edge_index = []
+    for edge in tqdm(dataset.edge_index.t(), desc="Generating test dataset"):
+        if not test_mask[edge[0]]:
+            continue
+
+        new_edge_index.append(
+            torch.tensor(
+                [
+                    edge[0].item(),
+                    edge[1].item(),
+                ]
+            )
+        )
+
+    new_edge_index = torch.stack(new_edge_index).t()
+    new_edge_label = torch.ones(new_edge_index.size(1), dtype=torch.int)
+
+    new_nodes = set(new_edge_index[0].tolist()).union(set(new_edge_index[1].tolist()))
+    new_nodes = sorted(new_nodes)
+
+    testing_nodes_in_new = set(new_edge_index[0].tolist())
+    testing_nodes_existing = dict()
+
+    # Generate adjacency list for existing edges
+    for edge in tqdm(
+        new_edge_index.t(), desc="Generating test dataset - Existing Edges"
+    ):
+        source, target = edge[0].item(), edge[1].item()
+
+        if source not in testing_nodes_existing:
+            testing_nodes_existing[source] = set()
+
+        testing_nodes_existing[source].add(target)
+
+    # Generate negative edges for the test dataset
+    negative_edge_index = []
+
+    for node in tqdm(
+        testing_nodes_in_new, desc="Generating test dataset - Negative Edges"
+    ):
+        node_counter = 0
+
+        if node not in new_nodes:
+            raise ValueError(f"Node {node} not found in new nodes.")
+
+        node_neighbors = testing_nodes_existing.get(node, set())
+
+        for target in random.sample(
+            new_nodes,
+            min(negative_edges_per_node + len(node_neighbors) + 1, len(new_nodes)),
+        ):
+            if node_counter >= negative_edges_per_node:
+                break
+
+            if target == node:
+                continue
+
+            if target not in node_neighbors:
+                negative_edge_index.append(torch.tensor([node, target]))
+                node_counter += 1
+
+    negative_edge_index = torch.stack(negative_edge_index).t()
+
+    new_edge_index = torch.cat([new_edge_index, negative_edge_index], dim=1)
+    new_edge_label = torch.cat(
+        [new_edge_label, torch.zeros(negative_edge_index.size(1), dtype=torch.int)],
+        dim=0,
+    )
+
+    # Create a mapping from the original node indices to the new indices
+    node_idx_mapping = dict()
+
+    for new_idx, og_idx in enumerate(new_nodes):
+        node_idx_mapping[og_idx] = new_idx
+
+    new_edge_index.apply_(lambda x: node_idx_mapping[x])
+
+    return Data(
+        x=dataset.x[new_nodes],
+        edge_index=new_edge_index.contiguous(),
+        edge_label=new_edge_label.contiguous(),
     )
