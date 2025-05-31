8a5dadd307e2743e95b106748503989f449add7a
NikolaP454 <97197562+NikolaP454@users.noreply.github.com>
2025-05-31 22:07:53 +0200

    Refactored ArxivDataset Class

--- src/datasets/arxiv_dataset.py
+++ src/datasets/arxiv_dataset.py
@@ -4,8 +4,11 @@ from torch_geometric.data import Data
 
 class ArxivDataset:
     def __init__(self, data: Data):
+        self.titles = data.x[:, 0]
+        self.abstracts = data.x[:, 1]
+
         self.data = Data(
-            x=data.x,
+            x=torch.Tensor(range(len(self.titles))),
             edge_index=data.edge_index,
         )
 
@@ -14,20 +17,24 @@ class ArxivDataset:
         self.data.edge_index = self.data.edge_index.contiguous()
 
     def __len__(self):
-        return len(self.data.x)
+        return len(self.titles)
 
     def __getitem__(self, idx):
         return {
-            "title-inputs": self.data.x[:, 0][idx],
-            "abstract-inputs": self.data.x[:, 1][idx],
-            "edge_index": self.data.edge_index,
+            "id": idx,
+            "title": self.titles[idx],
+            "abstract": self.abstracts[idx],
+            "cites": self.data.edge_index[1][self.data.edge_index[0] == idx].tolist(),
+            "is_cited_by": self.data.edge_index[0][
+                self.data.edge_index[1] == idx
+            ].tolist(),
         }
 
     def get_data(self):
         return self.data
 
     def get_title(self, idx):
-        return self.data.x[:, 0][idx]
+        return self.titles[idx]
 
     def get_abstract(self, idx):
-        return self.data.x[:, 1][idx]
+        return self.abstracts[idx]
