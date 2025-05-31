0fd28cbd12311cf035625017b7b52dd9c7458a39
NikolaP454 <97197562+NikolaP454@users.noreply.github.com>
2025-05-31 22:33:59 +0200

    Added Prompt-Based Model

---
+++ src/models/prompt_models.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+from transformers import DistilBertForSequenceClassification
+
+from .text_embedding_models import TextTokenizer
+
+
+class PromptLinkPredictionModel(nn.Module):
+    def __init__(
+        self, model_name: str = "distilbert-base-uncased", max_length: int = 512
+    ):
+        super(PromptLinkPredictionModel, self).__init__()
+
+        # Initialize the tokenizer and model
+        self.tokenizer = TextTokenizer(model_name=model_name, max_length=max_length)
+        self.model = DistilBertForSequenceClassification.from_pretrained(
+            model_name, num_labels=1
+        )
+
+        # Other initializations
+        self.max_length = max_length
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        self.to(self.device)
+
+    def forward(self, texts):
+        inputs = self.tokenizer(texts)
+        outputs = self.model(**inputs)
+
+        return outputs.logits
+
+    def to(self, device):
+        super(PromptLinkPredictionModel, self).to(device)
+
+        self.device = device
+        self.model.to(device)
+
+        return self
